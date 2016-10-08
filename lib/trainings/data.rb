57563e94ae4c0102d315379fd0c3aacc5fce1e4f
Kouhei Suzuki <kouhei.szk@gmail.com>
2016-10-08 23:01:17 +0900

    Fix bug

--- lib/trainings/data.rb
+++ lib/trainings/data.rb
@@ -5,7 +5,7 @@ module Trainings
     def self.create(lang:)
       CSV.generate do |csv|
         Klass.all.each do |klass|
-          klass.recipes.each do |recipe|
+          klass.recipes(lang: lang).each do |recipe|
             csv << [recipe.title, klass.id]
           end
         end
