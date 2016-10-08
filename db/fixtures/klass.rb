9dfc7de1d5dbad71fa9da110d759acf941d9b9be
Kouhei Suzuki <kouhei.szk@gmail.com>
2016-10-08 14:29:44 +0900

    クラス情報追加

---
+++ db/fixtures/klass.rb
@@ -0,0 +1,292 @@
+Klass.seed do |k|
+  k.id = 1
+  k.name_jp = "ゴーヤ"
+end
+Klass.seed do |k|
+  k.id = 2
+  k.name_jp = "にんじん,人参"
+end
+Klass.seed do |k|
+  k.id = 3
+  k.name_jp = "たまねぎ,玉ねぎ"
+end
+Klass.seed do |k|
+  k.id = 4
+  k.name_jp = "じゃがいも"
+end
+Klass.seed do |k|
+  k.id = 5
+  k.name_jp = "キャベツ"
+end
+Klass.seed do |k|
+  k.id = 6
+  k.name_jp = "きのこ"
+end
+Klass.seed do |k|
+  k.id = 7
+  k.name_jp = "大豆"
+end
+Klass.seed do |k|
+  k.id = 8
+  k.name_jp = "豆"
+end
+Klass.seed do |k|
+  k.id = 9
+  k.name_jp = "もやし"
+end
+Klass.seed do |k|
+  k.id = 10
+  k.name_jp = "にら"
+end
+Klass.seed do |k|
+  k.id = 11
+  k.name_jp = "ピーマン"
+end
+Klass.seed do |k|
+  k.id = 12
+  k.name_jp = "とまと,トマト"
+end
+Klass.seed do |k|
+  k.id = 13
+  k.name_jp = "なす,ナス"
+end
+Klass.seed do |k|
+  k.id = 14
+  k.name_jp = "かぼちゃ,カボチャ,南瓜"
+end
+Klass.seed do |k|
+  k.id = 15
+  k.name_jp = "パプリカ"
+end
+Klass.seed do |k|
+  k.id = 16
+  k.name_jp = "ズッキーニ"
+end
+Klass.seed do |k|
+  k.id = 17
+  k.name_jp = "オクラ"
+end
+Klass.seed do |k|
+  k.id = 18
+  k.name_jp = "きゅうり"
+end
+Klass.seed do |k|
+  k.id = 19
+  k.name_jp = "とうもろこし,トウモロコシ"
+end
+Klass.seed do |k|
+  k.id = 20
+  k.name_jp = "モロヘイヤ"
+end
+Klass.seed do |k|
+  k.id = 21
+  k.name_jp = "生姜,しょうが"
+end
+Klass.seed do |k|
+  k.id = 22
+  k.name_jp = "貝"
+end
+Klass.seed do |k|
+  k.id = 23
+  k.name_jp = "ほたて,ホタテ,帆立"
+end
+Klass.seed do |k|
+  k.id = 24
+  k.name_jp = "ムール貝"
+end
+Klass.seed do |k|
+  k.id = 25
+  k.name_jp = "牡蠣"
+end
+Klass.seed do |k|
+  k.id = 26
+  k.name_jp = "かに,カニ,蟹"
+end
+Klass.seed do |k|
+  k.id = 27
+  k.name_jp = "たまご,タマゴ,卵"
+end
+Klass.seed do |k|
+  k.id = 28
+  k.name_jp = "とうふ,豆腐,油揚げ"
+end
+Klass.seed do |k|
+  k.id = 29
+  k.name_jp = "なっとう,納豆"
+end
+Klass.seed do |k|
+  k.id = 30
+  k.name_jp = "こんにゃく"
+end
+Klass.seed do |k|
+  k.id = 31
+  k.name_jp = "たけのこ"
+end
+Klass.seed do |k|
+  k.id = 32
+  k.name_jp = "アスパラガス"
+end
+Klass.seed do |k|
+  k.id = 33
+  k.name_jp = "アボカド"
+end
+Klass.seed do |k|
+  k.id = 34
+  k.name_jp = "レタス"
+end
+Klass.seed do |k|
+  k.id = 35
+  k.name_jp = "大根"
+end
+Klass.seed do |k|
+  k.id = 36
+  k.name_jp = "白菜"
+end
+Klass.seed do |k|
+  k.id = 37
+  k.name_jp = "里芋"
+end
+Klass.seed do |k|
+  k.id = 38
+  k.name_jp = "さつまいも"
+end
+Klass.seed do |k|
+  k.id = 39
+  k.name_jp = "長芋,山芋"
+end
+Klass.seed do |k|
+  k.id = 40
+  k.name_jp = "ブロッコリー"
+end
+Klass.seed do |k|
+  k.id = 41
+  k.name_jp = "チンゲンサイ,青梗菜"
+end
+Klass.seed do |k|
+  k.id = 42
+  k.name_jp = "セロリ"
+end
+Klass.seed do |k|
+  k.id = 43
+  k.name_jp = "かぶ"
+end
+Klass.seed do |k|
+  k.id = 44
+  k.name_jp = "カリフラワー"
+end
+Klass.seed do |k|
+  k.id = 45
+  k.name_jp = "長ねぎ"
+end
+Klass.seed do |k|
+  k.id = 46
+  k.name_jp = "ほうれん草,小松菜"
+end
+Klass.seed do |k|
+  k.id = 47
+  k.name_jp = "にんにく"
+end
+Klass.seed do |k|
+  k.id = 48
+  k.name_jp = "春菊"
+end
+Klass.seed do |k|
+  k.id = 49
+  k.name_jp = "グリーンピース"
+end
+Klass.seed do |k|
+  k.id = 50
+  k.name_jp = "クレソン"
+end
+Klass.seed do |k|
+  k.id = 51
+  k.name_jp = "牛肉"
+end
+Klass.seed do |k|
+  k.id = 52
+  k.name_jp = "豚肉,ぶた肉"
+end
+Klass.seed do |k|
+  k.id = 53
+  k.name_jp = "鶏肉,とり肉,鶏もも肉,鶏むね肉,手羽先"
+end
+Klass.seed do |k|
+  k.id = 54
+  k.name_jp = "挽き肉"
+end
+Klass.seed do |k|
+  k.id = 55
+  k.name_jp = "鮭,さけ,しゃけ,サーモン"
+end
+Klass.seed do |k|
+  k.id = 56
+  k.name_jp = "いわし,イワシ,鰯"
+end
+Klass.seed do |k|
+  k.id = 57
+  k.name_jp = "さば,鯖"
+end
+Klass.seed do |k|
+  k.id = 58
+  k.name_jp = "あじ,鯵"
+end
+Klass.seed do |k|
+  k.id = 59
+  k.name_jp = "ぶり,ブリ,鰤"
+end
+Klass.seed do |k|
+  k.id = 60
+  k.name_jp = "さんま,サンマ,秋刀魚"
+end
+Klass.seed do |k|
+  k.id = 61
+  k.name_jp = "たい,鯛"
+end
+Klass.seed do |k|
+  k.id = 62
+  k.name_jp = "マグロ,鮪"
+end
+Klass.seed do |k|
+  k.id = 63
+  k.name_jp = "ひらめ,ヒラメ,平目"
+end
+Klass.seed do |k|
+  k.id = 64
+  k.name_jp = "かれい"
+end
+Klass.seed do |k|
+  k.id = 65
+  k.name_jp = "わかさぎ,ワカサギ"
+end
+Klass.seed do |k|
+  k.id = 66
+  k.name_jp = "たら,鱈"
+end
+Klass.seed do |k|
+  k.id = 67
+  k.name_jp = "うなぎ,あなご,アナゴ,穴子"
+end
+Klass.seed do |k|
+  k.id = 68
+  k.name_jp = "かつお,カツオ,鰹"
+end
+Klass.seed do |k|
+  k.id = 69
+  k.name_jp = "えび,エビ,海老"
+end
+Klass.seed do |k|
+  k.id = 70
+  k.name_jp = "いか,イカ"
+end
+Klass.seed do |k|
+  k.id = 71
+  k.name_jp = "たこ,タコ,蛸"
+end
+Klass.seed do |k|
+  k.id = 72
+  k.name_jp = "わかめ,こんぶ,昆布,ひじき,海藻"
+end
+Klass.seed do |k|
+  k.id = 73
+  k.name_jp = "チーズ"
+end
