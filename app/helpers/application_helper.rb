ddb0bafd7b75ef2dd2dfaa7538352b5ee24076f2
Kouhei Suzuki <kouhei.szk@gmail.com>
2016-10-09 12:29:20 +0900

    デバッグモードのリンクが動作するようにする

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def url_added_param(url, param_name, param_value)
+    uri = URI::parse(url)
+    query_array = uri.query.present? ? URI::decode_www_form(uri.query) : []
+    query_hash = Hash[query_array]
+    query_hash[param_name] = param_value
+
+    uri.query = query_hash.to_param
+    uri.to_s
+  end
 end
