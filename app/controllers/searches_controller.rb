3927336ee247a4d85b3acf61c15aed50a192da74
Kouhei Suzuki <kouhei.szk@gmail.com>
2016-10-09 10:13:50 +0900

    おすすめが1件しか表示されないようにする

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,13 +9,13 @@ class SearchesController < ApplicationController
       @enough = Klass.where(id: @materials.map(&:id))
       @poor = Klass.where.not(id: @materials.map(&:id))
 
-      enough_keywords = @enough.map(&:"name_#{I18n.locale}").map { |n| n.split(?,).map(&:strip) }.flatten.uniq
-      poor_keywords = @poor.map(&:"name_#{I18n.locale}").map { |n| n.split(?,).map(&:strip) }.flatten.uniq
+      enough_keywords = @enough.map(&:names).map { |n| n.split(?,).map(&:strip) }.flatten.uniq
+      poor_keywords = @poor.map(&:names).map { |n| n.split(?,).map(&:strip) }.flatten.uniq
 
       enough_recipe_ids = Recipe.contain_keywords(enough_keywords).eq_lang(I18n.locale).pluck(:id)
       poor_recipe_ids = Recipe.contain_keywords(poor_keywords).eq_lang(I18n.locale).pluck(:id)
       suggestion_recipe_ids = poor_recipe_ids - enough_recipe_ids
-      @suggestions = Recipe.where(id: suggestion_recipe_ids.sample(2))
+      @recommendation = Recipe.where(id: suggestion_recipe_ids).where.not(image_url: nil).sample(1).first
     end
   end
 
