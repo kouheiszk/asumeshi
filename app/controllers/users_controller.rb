0917c126aba07c2c3fea9f04ce44dae40b3b987c
Kouhei Suzuki <kouhei.szk@gmail.com>
2017-01-21 03:09:53 +0900

    アレルギーを登録できるようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,13 +62,13 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.fetch(:user, {})
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.fetch(:user, {})
+    params.require(:user).permit(:name, {allergy_ids: []})
+  end
 end
---
+++ db/fixtures/allergies.rb
@@ -0,0 +1,28 @@
+Allergy.seed do |a|
+  a.id = 1
+  a.name = '乳'
+end
+Allergy.seed do |a|
+  a.id = 2
+  a.name = '卵'
+end
+Allergy.seed do |a|
+  a.id = 3
+  a.name = '小麦'
+end
+Allergy.seed do |a|
+  a.id = 4
+  a.name = 'そば'
+end
+Allergy.seed do |a|
+  a.id = 5
+  a.name = '落花生'
+end
+Allergy.seed do |a|
+  a.id = 6
+  a.name = 'えび'
+end
+Allergy.seed do |a|
+  a.id = 7
+  a.name = 'かに'
+end
