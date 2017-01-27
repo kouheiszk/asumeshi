518e2991827950de81897789102cf2b06ec16537
Kouhei Suzuki <kouhei.szk@gmail.com>
2017-01-28 07:32:42 +0900

    jsが正しくどうさするようにする

--- app/assets/javascripts/kondate.js
+++ app/assets/javascripts/kondate.js
@@ -1,7 +1,7 @@
-$(function() {
+$(document).on('turbolinks:load', function () {
     var $kondate_title_field = $('.js-kondate-title-field');
     if ($kondate_title_field.length) {
-        $('.js-kondate-ate-button').on('click', function() {
+        $('.js-kondate-ate-button').on('click', function () {
             var title = $kondate_title_field.val() + ' ' + $(this).data('title');
             var keywords = title.split(' ').filter(function (element, index, self) {
                 return self.indexOf(element) === index;
