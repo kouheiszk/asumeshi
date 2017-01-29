c476926d982d46d0aed391d55c104414a855348f
Kouhei Suzuki <kouhei.szk@gmail.com>
2017-01-30 00:05:55 +0900

    デザインを修正

--- app/assets/javascripts/kondate.js
+++ app/assets/javascripts/kondate.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function () {
-    var $kondate_title_field = $('.js-kondate-title-field');
+    var $kondate_title_field = $('.js-kondate-query-field');
     if ($kondate_title_field.length) {
         $('.js-kondate-ate-button').on('click', function () {
             var title = $kondate_title_field.val() + ' ' + $(this).data('title');
